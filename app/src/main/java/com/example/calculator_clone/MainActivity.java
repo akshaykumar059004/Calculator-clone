3dd98bb4366bcb3227f8a6ca299cef2af6a18fd7
Akshay <akshaykumar059004@gmail.com>
2025-01-09 22:31:47 +0530

    launched successfully to physical device

--- app/src/main/java/com/example/calculator_clone/MainActivity.java
+++ app/src/main/java/com/example/calculator_clone/MainActivity.java
@@ -1,5 +1,13 @@
 package com.example.calculator_clone;
 
-public class MainActivity {
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.acticity_main);
+    }
 
 }
